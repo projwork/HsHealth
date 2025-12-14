aae9a0b0954c2d323ed9b7c7136dde19e0d2057d
Biruk Anose <biruk4work@gmail.com>
2025-12-14 19:58:04 +0300

    feat: add sqllite connection string and register db context on program file

--- PatientMenu/PatientMenu.Api/Program.cs
+++ PatientMenu/PatientMenu.Api/Program.cs
@@ -1,11 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PatientMenu.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MenuDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
